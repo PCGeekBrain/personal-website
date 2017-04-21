50a8fd47bad790d4c8fabb137d9fa5749cb26294
Mendel Hornbacher <Mendel Hornbacher>
2017-04-20 23:00:23 -0400

    Updated hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,18 +89,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': 'mySQL.cnf',
-        },
-    }
     'test': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': secret_settings.dbs_name,
+        'USER': secret_settings.dbs_user,
+        'PASSWORD': secret_settings.dbs_password,
+        'HOST': secret_settings.dbs_host,   # Or an IP Address that your DB is hosted on
+        'PORT': secret_settings.dbs_port,
+    },
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
