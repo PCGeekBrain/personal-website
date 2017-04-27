866711b6c9c9318567b4c8a0fa7708f7b0f6ab0f
Mendel Hornbacher <Mendel Hornbacher>
2017-04-27 11:37:48 -0400

    made content blank and removed migrations from Version Control

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 def post_upload_location(instance, filename):
-    return "/images/blog/{}/{}".format(instance.slug, filename)
+    return "images/blog/{}/{}".format(instance.slug, filename)
 
 # Create your models here.
 class Post(models.Model):
     """The block of content detailing the post"""
     title = models.CharField(max_length=150, help_text="Title for post, also used for url")
-    content = models.TextField()
+    content = models.TextField(blank=True, null=True)
     header_image = models.ImageField(upload_to=post_upload_location, blank=True, null=True, height_field="image_height", width_field="image_width")
     image_height = models.IntegerField(default=0)
     image_width = models.IntegerField(default=0)
@@ -15,3 +15,6 @@ class Post(models.Model):
 
     def getPath(self):
         return self.title.replace(" ", "-")
+    
+    def getTitle(self, path):
+        return path.replace("-", " ")
