338f82270c0f8aeccb95f4349ee5f51cfa1c734b
Mendel Hornbacher <Mendel Hornbacher>
2017-04-28 13:25:38 -0400

    migration

--- blog/misc.py
+++ blog/misc.py
@@ -3,6 +3,9 @@ from django.conf import settings
 import random
 import os
 
-def random_stock_image():
-    images = os.listdir(os.path.join(settings.BASE_DIR, 'common_static/stock_img'))
-    return "/static/stock_img/{}".format(random.choice(images))
+class random_stock_image:
+    def __init__(self):
+        print('read folder')
+        self.images = os.listdir(os.path.join(settings.BASE_DIR, 'common_static/stock_img'))
+    def generate(self):
+        return "/static/stock_img/{}".format(random.choice(self.images))
